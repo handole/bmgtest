0b95de5023c5dc199fc22be07aff28a5dcd1d8d5
handole <denihhandoko@gmail.com>
2021-08-19 13:44:54 +0700

    initial master

---
+++ test1.py
@@ -0,0 +1,11 @@
+n = 50
+s = ['Frontend', 'Backend']
+list_a = []
+list_b = []
+list_c = []
+
+for i in range(15):
+    list_a.append(i+1)
+list_a[2] = 'Frontend'
+list_a[4] = 'Backend'
+print(list_a)
